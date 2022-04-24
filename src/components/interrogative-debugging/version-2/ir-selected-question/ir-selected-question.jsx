4881339b1e8d1a25807cc8ba6ffbb5d8896f275f
Adina Deiner <deinerad@fim.uni-passau.de>
2022-04-24 16:24:34 +0200

    Add feedback buttons

--- src/components/interrogative-debugging/version-2/ir-selected-question/ir-selected-question.jsx
+++ src/components/interrogative-debugging/version-2/ir-selected-question/ir-selected-question.jsx
@@ -1,19 +1,58 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {injectIntl} from 'react-intl';
-import {FormattedHTMLMessage} from 'react-intl';
+import {injectIntl, FormattedHTMLMessage} from 'react-intl';
 import classNames from 'classnames';
+import bindAll from 'lodash.bindall';
 
 import {QuestionV2 as Question} from 'scratch-ir';
 
 import styles from './ir-selected-question.css';
 import irStyles from '../ir-styles.css';
 
+import thinkBubble from '../icons/icon--think-bubble.svg';
+import positiveFeedback from '../icons/icon--positive-feedback.svg';
+import neutralFeedback from '../icons/icon--neutral-feedback.svg';
+import negativeFeedback from '../icons/icon--negative-feedback.svg';
+
+const Feedback = Object.freeze({
+    POSITIVE: 1,
+    NEUTRAL: 0,
+    NEGATIVE: -1
+});
+
 class IRSelectedQuestion extends React.Component {
 
     constructor (props) {
         super(props);
         this.state = {};
+
+        bindAll(this, [
+            'handleFeedback',
+            'handlePositiveFeedback',
+            'handleNeutralFeedback',
+            'handleNegativeFeedback'
+        ]);
+    }
+    
+    handleFeedback (value) {
+        if (this.props.selectedQuestion.feedback === value) {
+            this.props.selectedQuestion.feedback = null;
+        } else {
+            this.props.selectedQuestion.feedback = value;
+        }
+        this.forceUpdate();
+    }
+
+    handlePositiveFeedback () {
+        this.handleFeedback(Feedback.POSITIVE);
+    }
+
+    handleNeutralFeedback () {
+        this.handleFeedback(Feedback.NEUTRAL);
+    }
+
+    handleNegativeFeedback () {
+        this.handleFeedback(Feedback.NEGATIVE);
     }
 
     render () {
@@ -23,16 +62,46 @@ class IRSelectedQuestion extends React.Component {
         return (
             <div>
                 {selectedQuestion ? (
-                    <div
-                        className={classNames(
-                            styles.selectedQuestion,
-                            irStyles[`color-${selectedQuestion.color.replace('#', '')}`]
-                        )}
-                    >
-                        <FormattedHTMLMessage
-                            tagName="div"
-                            {...selectedQuestion.message}
-                        />
+                    <div>
+                        <div
+                            className={classNames(
+                                styles.selectedQuestion,
+                                irStyles[`color-${selectedQuestion.color.replace('#', '')}`]
+                            )}
+                        >
+                            <FormattedHTMLMessage
+                                tagName="div"
+                                {...selectedQuestion.message}
+                            />
+                        </div>
+
+                        <div className={styles.feedback}>
+                            <img
+                                className={classNames(styles.thinkBubble)}
+                                src={thinkBubble}
+                            />
+                            <img
+                                className={selectedQuestion.feedback === Feedback.POSITIVE ?
+                                    classNames(styles.selectedFeedbackButton, styles.feedbackButton) :
+                                    classNames(styles.feedbackButton)}
+                                onClick={this.handlePositiveFeedback}
+                                src={positiveFeedback}
+                            />
+                            <img
+                                className={selectedQuestion.feedback === Feedback.NEUTRAL ?
+                                    classNames(styles.selectedFeedbackButton, styles.feedbackButton) :
+                                    classNames(styles.feedbackButton)}
+                                onClick={this.handleNeutralFeedback}
+                                src={neutralFeedback}
+                            />
+                            <img
+                                className={selectedQuestion.feedback === Feedback.NEGATIVE ?
+                                    classNames(styles.selectedFeedbackButton, styles.feedbackButton) :
+                                    classNames(styles.feedbackButton)}
+                                onClick={this.handleNegativeFeedback}
+                                src={negativeFeedback}
+                            />
+                        </div>
                     </div>
                 ) : null}
             </div>
