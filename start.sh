477f982c841a6f2612f191522c182d02b2c118c6
Patric Feldmeier <feldmeie@fim.uni-passau.de>
2023-09-12 17:20:01 +0200

    Add run script II

--- start.sh
+++ start.sh
@@ -1,5 +1,10 @@
 # !/bin/bash
 
+echo "Install Dependencies"
 npm install
+
+echo "Remove test files of whisker-main due to compiling issues"
 rm -rf node_modules/whisker-main/test
+
+echo "Start "
 npm start
