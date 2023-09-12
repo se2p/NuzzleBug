efebcdad35ef9f753d3afba8b5268d601888bfa9
Patric Feldmeier <feldmeie@fim.uni-passau.de>
2023-09-12 17:31:41 +0200

    Fix package-lock and start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "Install Dependencies"
 npm install
 
 echo "Remove test files of whisker-main due to compiling issues"
-rm -rf node_modules/whisker-main/test
+rm -rf node_modules/whisker-main/whisker-main/test
 
 echo "Start "
 npm start
