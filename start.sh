8c0bb693708cd0295c10c6dec1494d8ab4bf9487
Patric Feldmeier <feldmeie@fim.uni-passau.de>
2023-09-13 12:25:02 +0200

    Use yarn instead of npm

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 # !/bin/bash
 
 echo "Install Dependencies"
-npm ci
+yarn install
 
 echo "Remove test files of whisker-main due to compiling issues"
 rm -rf node_modules/whisker-main/whisker-main/test
 
-echo "Start "
+echo "Start NuzzleBug"
 npm start
