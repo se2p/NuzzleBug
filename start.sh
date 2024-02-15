3ccfa99df13d3daf4b02cec1785770c4a368acde
Patric Feldmeier <feldmeie@fim.uni-passau.de>
2024-02-15 15:29:53 +0100

    Update whisker-main reference which fixes jest issue when building tutorials

--- start.sh
+++ start.sh
@@ -3,8 +3,5 @@
 echo "Install Dependencies"
 yarn install
 
-echo "Remove test files of whisker-main due to compiling issues"
-rm -rf node_modules/whisker-main/whisker-main/test
-
-echo "Start NuzzleBug"
+echo "Start Tutorials"
 npm start
