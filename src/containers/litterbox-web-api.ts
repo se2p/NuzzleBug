704fe3ab03b05a293b21cf2d875ff7af5effb5de
Benedikt Fein <fein@fim.uni-passau.de>
2025-07-07 19:37:03 +0200

    api for LitterBox analysis
    Reflects the API endpoint already present in LitterBox-Web.

--- src/containers/litterbox-web-api.ts
+++ src/containers/litterbox-web-api.ts
@@ -21,15 +21,41 @@ interface RawLitterBoxTutorialHint {
 
 const baseUrl = process.env.LITTERBOX_BASE_URL;
 
+type UrlParams = {[key: string]: unknown};
+
+/**
+ * Filters the URL parameters to include only non-empty, non-null/undefined ones.
+ *
+ * @param params - A set of URL parameter key/value pairs.
+ * @returns Only non-empty, non-null/undefined parameters.
+ */
+const cleanURLSearchParams = (params: UrlParams): URLSearchParams => {
+    const filtered = {};
+
+    Object.keys(params)
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        .forEach((key, _) => {
+            const value = params[key];
+            // eslint-disable-next-line no-undefined
+            if (value !== '' && value !== null && value !== undefined) {
+                filtered[key] = value;
+            }
+        });
+
+    return new URLSearchParams(filtered);
+};
+
 /**
  * Sends a POST request to LitterBox with a JSON body and a JSON response.
  *
  * @param endpoint - The REST endpoint of LitterBox-Web.
  * @param body - The body sent to LitterBox-Web.
+ * @param urlParams - Optional request parameters to be added to the URL.
  * @returns The decoded JSON response.
  */
-const postJsonWithJsonResponse = async <T, R>(endpoint: string, body: T): Promise<R> => {
-    const response = await fetch(`${baseUrl}/${endpoint}`, {
+const postJsonWithJsonResponse = async <T, R>(endpoint: string, body: T, urlParams?: UrlParams): Promise<R> => {
+    const queryParams = urlParams ? `?${cleanURLSearchParams(urlParams).toString()}` : '';
+    const response = await fetch(`${baseUrl}/${endpoint}${queryParams}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -63,4 +89,28 @@ export const getTutorialFeedback = async (
         type: hint.type,
         codeSnippet: hint.scratchBlocksCode
     }));
+};
+
+interface LitterBoxHint {
+    blockId: string;
+    issueType: string;
+    finderName: string;
+    translatedFinderName: string;
+    issueHint: string;
+    hatBlockId: string;
+}
+
+/**
+ * Runs the LitterBox analysis.
+ *
+ * @param program - A Scratch program.
+ * @param detectors - The linters to run in the LitterBox analysis.
+ * @param locale - The language for the hint text (`en` or `de`).
+ * @returns A list of LitterBox findings.
+ */
+export const getLitterBoxAnalysis = (
+    program: ScratchProjectJson, detectors?: string, locale?: string
+): Promise<LitterBoxHint[]> => {
+    const urlParams = {locale: locale, detectors: detectors};
+    return postJsonWithJsonResponse(`linter/analyze`, program, urlParams);
 };
